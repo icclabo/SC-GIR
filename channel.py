1255f8b1cfb106d436db32ee710ce3c8159238e8
Senura Hansaja <ifire810@gmail.com>
2025-03-04 04:32:42 +0000

    channel

--- channel.py
+++ channel.py
@@ -6,18 +6,21 @@ import pytorch_lightning as pl
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import math
+
+
 
 class Channels():
 
     def AWGN(self, Tx_sig, n_var):
-        Rx_sig = Tx_sig + torch.normal(0, n_var, size=Tx_sig.shape).to(device)
+        Rx_sig = Tx_sig + torch.normal(0, n_var, size=Tx_sig.shape).to('cuda')
         return Rx_sig
 
     def Rayleigh(self, Tx_sig, n_var):
         shape = Tx_sig.shape
-        H_real = torch.normal(0, math.sqrt(1/2), size=[1]).to(device)
-        H_imag = torch.normal(0, math.sqrt(1/2), size=[1]).to(device)
-        H = torch.Tensor([[H_real, -H_imag], [H_imag, H_real]]).to(device)
+        H_real = torch.normal(0, math.sqrt(1/2), size=[1]).to('cuda')
+        H_imag = torch.normal(0, math.sqrt(1/2), size=[1]).to('cuda')
+        H = torch.Tensor([[H_real, -H_imag], [H_imag, H_real]]).to('cuda')
         Tx_sig = torch.matmul(Tx_sig.view(shape[0], -1, 2), H)
         Rx_sig = self.AWGN(Tx_sig, n_var)
         # Channel estimation
@@ -29,9 +32,9 @@ class Channels():
         shape = Tx_sig.shape
         mean = math.sqrt(K / (K + 1))
         std = math.sqrt(1 / (K + 1))
-        H_real = torch.normal(mean, std, size=[1]).to(device)
-        H_imag = torch.normal(mean, std, size=[1]).to(device)
-        H = torch.Tensor([[H_real, -H_imag], [H_imag, H_real]]).to(device)
+        H_real = torch.normal(mean, std, size=[1]).to('cuda')
+        H_imag = torch.normal(mean, std, size=[1]).to('cuda')
+        H = torch.Tensor([[H_real, -H_imag], [H_imag, H_real]]).to('cuda')
         Tx_sig = torch.matmul(Tx_sig.view(shape[0], -1, 2), H)
         Rx_sig = self.AWGN(Tx_sig, n_var)
         # Channel estimation
